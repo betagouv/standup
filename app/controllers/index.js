b3aec882f8eba2183058eee828e39306df5c17a9
gregoirenovel <log@kinko.fr>
2017-06-20 17:24:12 +0200

    Add filtered/sorted startups computed properties

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,5 +7,37 @@ export default Ember.Controller.extend({
     start: function() {
       this.set('state', 'startups')
     }
-  }
+  },
+
+  shuffle: function(collection) {
+    var currentIndex = collection.length,
+        temporaryValue,
+        randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = collection[currentIndex];
+      collection[currentIndex] = collection[randomIndex];
+      collection[randomIndex] = temporaryValue;
+    }
+
+    return collection;
+  },
+
+  activeStartups: Ember.computed('model', function() {
+    return this.get('model').rejectBy('status', 'death')
+  }),
+  incubateurStartups: Ember.computed('activeStartups', function() {
+    return this.get('activeStartups').rejectBy('status', 'success')
+  }),
+  friendsStartups: Ember.computed.filterBy('activeStartups', 'status', 'success'),
+  shuffledIncubateurStartups: Ember.computed('incubateurStartups', function() {
+    return this.shuffle(this.get('incubateurStartups'));
+  }),
+  startups: Ember.computed.union('shuffledIncubateurStartups', 'friendsStartups')
 });
