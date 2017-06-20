789f42f0cb1b2d33070d4c37c538f361466c1e2c
gregoirenovel <log@kinko.fr>
2017-06-20 19:42:14 +0200

    Add a timer

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,14 +3,44 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   state: 'home',
   startupIndex: 0,
+  timer: null,
+  secondsLeft: null,
+  minutesLeft: null,
 
   actions: {
     start: function() {
+      this.setTimer();
       this.set('state', 'startups')
     },
 
     nextStartup: function() {
-      this.set('startupIndex', this.get('startupIndex') + 1);
+      clearInterval(this.get('timer'));
+
+      if (this.get('startupIndex') < (this.get('startups.length') - 1)) {
+        this.setTimer();
+        this.set('startupIndex', this.get('startupIndex') + 1);
+      } else {
+        this.set('startupIndex', 0);
+        this.set('state', 'home');
+      }
+    }
+  },
+
+  setTimer: function() {
+    var endtime = this.endtime(65),
+        timer = setInterval(function() { this.tick(endtime); }.bind(this), 1000);
+
+    this.set('timer', timer);
+    this.tick(endtime);
+  },
+
+  tick: function(endtime) {
+    var remainingTime = this.getRemainingTime(endtime);
+    this.set('secondsLeft', remainingTime.seconds);
+    this.set('minutesLeft', remainingTime.minutes);
+
+    if (remainingTime.total <= 0) {
+      this.send('nextStartup');
     }
   },
 
@@ -34,6 +64,25 @@ export default Ember.Controller.extend({
     return collection;
   },
 
+  endtime: function(seconds) {
+    var time = new Date();
+    time.setSeconds(time.getSeconds() + seconds)
+
+    return time;
+  },
+
+  getRemainingTime: function(endtime) {
+    var time = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((time / 1000) % 60),
+        minutes = Math.floor((time / 1000 / 60) % 60);
+
+    return {
+      'total': time,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  },
+
   activeStartups: Ember.computed('model', function() {
     return this.get('model').rejectBy('status', 'death')
   }),
@@ -47,5 +96,11 @@ export default Ember.Controller.extend({
   startups: Ember.computed.union('shuffledIncubateurStartups', 'friendsStartups'),
   currentStartup: Ember.computed('startups', 'startupIndex', function() {
     return this.get('startups')[this.get('startupIndex')];
+  }),
+  formattedSecondsLeft: Ember.computed('secondsLeft', function() {
+    return ("00" + String(this.get('secondsLeft'))).slice(-2);
+  }),
+  formattedMinutesLeft: Ember.computed('minutesLeft', function() {
+    return ("00" + String(this.get('minutesLeft'))).slice(-2);
   })
 });
