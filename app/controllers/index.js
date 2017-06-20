beed42fa62cd2870485c415de90ed9f69ca19fd2
gregoirenovel <log@kinko.fr>
2017-06-20 16:45:00 +0200

    Add an action on the start button

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  state: 'home'
+  state: 'home',
+
+  actions: {
+    start: function() {
+      this.set('state', 'startups')
+    }
+  }
 });
