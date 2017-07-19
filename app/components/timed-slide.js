938af75fc7c97d65469c2005861f0bc93ddc3716
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2017-07-19 17:17:57 +0200

    Add right arrow binding to next

--- app/components/timed-slide.js
+++ app/components/timed-slide.js
@@ -3,7 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['container'],
 
+  init() {
+    this._super(...arguments);
+    document.addEventListener('keyup', (event) => { this.keyUp(event); });
+  },
+
   click() {
+    this.get('next')();
+  },
+
+  keyUp(event) {
+    if(event.key !== 'ArrowRight') { return; }
     this.get('next')();
   }
 });
