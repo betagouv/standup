f7101c9d2ad83649f2838c20b3475cd15e434664
Florian Delezenne <del.florian@gmail.com>
2017-09-20 11:33:00 +0200

    Manually add OpenFisca to the standup

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('startup');
+    this.get('store').createRecord('startup', {
+      id: 'openfisca',
+      name: 'OpenFisca',
+      pitch: 'Rendre le droit calculable',
+      status: 'success'
+    });
+
+    return this.get('store').findAll('startup', { reload: true });
   }
 });
