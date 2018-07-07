727d6c6e87497d2f975a0efec19c8dc0c2ef8530
gregoirenovel <log@kinko.fr>
2018-07-07 10:31:03 +0200

    Support the incubator attribute

--- app/models/startup.js
+++ app/models/startup.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   pitch: DS.attr('string'),
-  status: DS.attr('string')
+  status: DS.attr('string'),
+  incubator: DS.attr('string')
 });
